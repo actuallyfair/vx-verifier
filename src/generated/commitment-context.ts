8b7bdd2fcd03537ab942fb19d2748663dddd500c
Hussein Zein <hussein@provablyhonest.com>
2024-06-23 12:29:15 +0900

    Add a prefixed partion commitment

--- src/generated/commitment-context.ts
+++ src/generated/commitment-context.ts
@@ -9,9 +9,16 @@ export interface Sha256Commitment {
 export interface Sha256Chain {
 }
 
+/** This allows you to use the same prefix for different groups of commitments, so each commitment will have the same .startsWith */
+export interface PrefixPartioned {
+  prefix: Uint8Array;
+  commitment: CommitmentContext | undefined;
+}
+
 export interface CommitmentContext {
   sha256Commitment?: Sha256Commitment | undefined;
   sha256Chain?: Sha256Chain | undefined;
+  prefixPartioned?: PrefixPartioned | undefined;
 }
 
 function createBaseSha256Commitment(): Sha256Commitment {
@@ -100,8 +107,84 @@ export const Sha256Chain = {
   },
 };
 
+function createBasePrefixPartioned(): PrefixPartioned {
+  return { prefix: new Uint8Array(0), commitment: undefined };
+}
+
+export const PrefixPartioned = {
+  encode(message: PrefixPartioned, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.prefix.length !== 0) {
+      writer.uint32(10).bytes(message.prefix);
+    }
+    if (message.commitment !== undefined) {
+      CommitmentContext.encode(message.commitment, writer.uint32(18).fork()).ldelim();
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): PrefixPartioned {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBasePrefixPartioned();
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          if (tag !== 10) {
+            break;
+          }
+
+          message.prefix = reader.bytes();
+          continue;
+        case 2:
+          if (tag !== 18) {
+            break;
+          }
+
+          message.commitment = CommitmentContext.decode(reader, reader.uint32());
+          continue;
+      }
+      if ((tag & 7) === 4 || tag === 0) {
+        break;
+      }
+      reader.skipType(tag & 7);
+    }
+    return message;
+  },
+
+  fromJSON(object: any): PrefixPartioned {
+    return {
+      prefix: isSet(object.prefix) ? bytesFromBase64(object.prefix) : new Uint8Array(0),
+      commitment: isSet(object.commitment) ? CommitmentContext.fromJSON(object.commitment) : undefined,
+    };
+  },
+
+  toJSON(message: PrefixPartioned): unknown {
+    const obj: any = {};
+    if (message.prefix.length !== 0) {
+      obj.prefix = base64FromBytes(message.prefix);
+    }
+    if (message.commitment !== undefined) {
+      obj.commitment = CommitmentContext.toJSON(message.commitment);
+    }
+    return obj;
+  },
+
+  create<I extends Exact<DeepPartial<PrefixPartioned>, I>>(base?: I): PrefixPartioned {
+    return PrefixPartioned.fromPartial(base ?? ({} as any));
+  },
+  fromPartial<I extends Exact<DeepPartial<PrefixPartioned>, I>>(object: I): PrefixPartioned {
+    const message = createBasePrefixPartioned();
+    message.prefix = object.prefix ?? new Uint8Array(0);
+    message.commitment = (object.commitment !== undefined && object.commitment !== null)
+      ? CommitmentContext.fromPartial(object.commitment)
+      : undefined;
+    return message;
+  },
+};
+
 function createBaseCommitmentContext(): CommitmentContext {
-  return { sha256Commitment: undefined, sha256Chain: undefined };
+  return { sha256Commitment: undefined, sha256Chain: undefined, prefixPartioned: undefined };
 }
 
 export const CommitmentContext = {
@@ -111,6 +194,9 @@ export const CommitmentContext = {
     }
     if (message.sha256Chain !== undefined) {
       Sha256Chain.encode(message.sha256Chain, writer.uint32(18).fork()).ldelim();
+    }
+    if (message.prefixPartioned !== undefined) {
+      PrefixPartioned.encode(message.prefixPartioned, writer.uint32(26).fork()).ldelim();
     }
     return writer;
   },
@@ -136,6 +222,13 @@ export const CommitmentContext = {
 
           message.sha256Chain = Sha256Chain.decode(reader, reader.uint32());
           continue;
+        case 3:
+          if (tag !== 26) {
+            break;
+          }
+
+          message.prefixPartioned = PrefixPartioned.decode(reader, reader.uint32());
+          continue;
       }
       if ((tag & 7) === 4 || tag === 0) {
         break;
@@ -149,6 +242,7 @@ export const CommitmentContext = {
     return {
       sha256Commitment: isSet(object.sha256Commitment) ? Sha256Commitment.fromJSON(object.sha256Commitment) : undefined,
       sha256Chain: isSet(object.sha256Chain) ? Sha256Chain.fromJSON(object.sha256Chain) : undefined,
+      prefixPartioned: isSet(object.prefixPartioned) ? PrefixPartioned.fromJSON(object.prefixPartioned) : undefined,
     };
   },
 
@@ -159,6 +253,9 @@ export const CommitmentContext = {
     }
     if (message.sha256Chain !== undefined) {
       obj.sha256Chain = Sha256Chain.toJSON(message.sha256Chain);
+    }
+    if (message.prefixPartioned !== undefined) {
+      obj.prefixPartioned = PrefixPartioned.toJSON(message.prefixPartioned);
     }
     return obj;
   },
@@ -174,9 +271,37 @@ export const CommitmentContext = {
     message.sha256Chain = (object.sha256Chain !== undefined && object.sha256Chain !== null)
       ? Sha256Chain.fromPartial(object.sha256Chain)
       : undefined;
+    message.prefixPartioned = (object.prefixPartioned !== undefined && object.prefixPartioned !== null)
+      ? PrefixPartioned.fromPartial(object.prefixPartioned)
+      : undefined;
     return message;
   },
 };
+
+function bytesFromBase64(b64: string): Uint8Array {
+  if (globalThis.Buffer) {
+    return Uint8Array.from(globalThis.Buffer.from(b64, "base64"));
+  } else {
+    const bin = globalThis.atob(b64);
+    const arr = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; ++i) {
+      arr[i] = bin.charCodeAt(i);
+    }
+    return arr;
+  }
+}
+
+function base64FromBytes(arr: Uint8Array): string {
+  if (globalThis.Buffer) {
+    return globalThis.Buffer.from(arr).toString("base64");
+  } else {
+    const bin: string[] = [];
+    arr.forEach((byte) => {
+      bin.push(globalThis.String.fromCharCode(byte));
+    });
+    return globalThis.btoa(bin.join(""));
+  }
+}
 
 type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
 
